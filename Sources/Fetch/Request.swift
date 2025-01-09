e6a7b160cfdc5fb4c43846ed03e48fe1603a9369
Guilherme Souza <grsouza@pm.me>
2025-01-09 07:24:33 -0300

    move parts to separate file

---
+++ Sources/Fetch/Request.swift
@@ -0,0 +1,68 @@
+//
+//  Request.swift
+//  Fetch
+//
+//  Created by Guilherme Souza on 08/01/25.
+//
+
+import Foundation
+
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
+
+/// Represents options for an HTTP request.
+public struct RequestOptions: @unchecked Sendable {
+  /// The HTTP method for the request (e.g., "GET", "POST", "PUT", etc.).
+  public let method: String
+
+  /// The body of the request. Supported types are (`Data`, `[UInt8]`, `String`, `FormData`, `URLSearchParams`, `any valid JSON object`, `any Encodable`).
+  public let body: Any?
+
+  /// A dictionary of HTTP headers to be included in the request.
+  public let headers: [String: String]
+
+  /// Initializes a new `RequestOptions` instance.
+  /// - Parameters:
+  ///   - method: The HTTP method for the request. Defaults to "GET".
+  ///   - body: The body of the request. Supported types are (`Data`, `[UInt8]`, `String`, `FormData`, `URLSearchParams`, `any valid JSON object`, `any Encodable`).
+  ///   - headers: A dictionary of HTTP headers. Defaults to an empty dictionary.
+  public init(
+    method: String = "GET",
+    body: (any Sendable)? = nil,
+    headers: [String: String] = [:]
+  ) {
+    self.method = method
+    self.body = body
+    self.headers = headers
+  }
+}
+
+/// Represents an HTTP request.
+public struct Request: Sendable {
+  /// The URL for the request.
+  public let url: URL
+
+  /// Optional `RequestOptions` for the request.
+  public let options: RequestOptions?
+
+  /// Initializes a new `Request` instance.
+  /// - Parameters:
+  ///   - url: The URL for the request.
+  ///   - options: Optional `RequestOptions` for the request.
+  public init(url: URL, options: RequestOptions? = nil) {
+    self.url = url
+    self.options = options
+  }
+}
+
+extension Request {
+  /// Initializes a new `Request` instance with a string URL.
+  /// - Parameters:
+  ///   - url: A string representation of the URL.
+  ///   - options: Optional `RequestOptions` for the request.
+  /// - Warning: This initializer force-unwraps the URL. Ensure the URL string is valid.
+  public init(url: String, options: RequestOptions? = nil) {
+    self.init(url: URL(string: url)!, options: options)
+  }
+}
---
+++ Sources/Fetch/Response.swift
@@ -0,0 +1,116 @@
+//
+//  Response.swift
+//  Fetch
+//
+//  Created by Guilherme Souza on 08/01/25.
+//
+
+import Foundation
+
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
+
+/// Represents an HTTP response.
+public struct Response: Sendable {
+  /// The URL of the response.
+  public let url: URL
+
+  /// The body of the response.
+  public let body: Body
+
+  /// A dictionary of HTTP headers received in the response.
+  public let headers: [String: String]
+
+  /// The HTTP status code of the response.
+  public let status: Int
+
+  /// The HTTP status text of the response.
+  public var statusText: String {
+    HTTPURLResponse.localizedString(forStatusCode: status)
+  }
+
+  /// Indicates whether the response status code is in the successful range (200-299).
+  public var ok: Bool {
+    200..<300 ~= status
+  }
+
+  /// Converts the response body to a string.
+  /// - Returns: The response body as a UTF-8 encoded string.
+  public func text() async -> String {
+    await String(decoding: body.collect(), as: UTF8.self)
+  }
+
+  /// Decodes the response body to a specified type.
+  /// - Parameters:
+  ///   - type: The type to decode the JSON into.
+  ///   - decoder: The JSON decoder to use. Defaults to global `Fetch.decoder`.
+  /// - Returns: The decoded object of type `T`.
+  /// - Throws: An error if decoding fails.
+  public func decode<T: Decodable & Sendable>(
+    as type: T.Type = T.self,
+    decoder: JSONDecoder? = nil
+  ) async throws -> T {
+    if T.self is Data.Type {
+      return await body.collect() as! T
+    } else if T.self is String.Type {
+      return await self.text() as! T
+    } else {
+      return try await (decoder ?? Fetch.decoder).decode(type, from: body.collect())
+    }
+  }
+
+  /// Decodes the response body as a JSON.
+  public func json() async throws -> Any {
+    try await JSONSerialization.jsonObject(with: body.collect())
+  }
+
+  public struct Body: AsyncSequence, Sendable {
+    public typealias AsyncIterator = AsyncStream<Data>.Iterator
+    public typealias Element = Data
+    public typealias Failure = Never
+
+    let stream: AsyncStream<Data>
+    let continuation: AsyncStream<Data>.Continuation
+
+    init() {
+      (stream, continuation) = AsyncStream.makeStream()
+    }
+
+    public func makeAsyncIterator() -> AsyncIterator {
+      stream.makeAsyncIterator()
+    }
+
+    public func collect() async -> Data {
+      await stream.reduce(into: Data()) { $0 += $1 }
+    }
+
+    func append(_ data: Data) {
+      continuation.yield(data)
+    }
+
+    func finalize() {
+      continuation.finish()
+    }
+  }
+
+}
+extension Response.Body {
+  public static func string(_ string: String, using encoding: String.Encoding = .utf8) -> Self {
+    let body = Self()
+    body.append(string.data(using: encoding)!)
+    body.finalize()
+    return body
+  }
+
+  public static func data(_ data: Data) -> Self {
+    let body = Self()
+    body.append(data)
+    body.finalize()
+    return body
+  }
+
+  public static func bytes(_ bytes: [UInt8]) -> Self {
+    .data(Data(bytes))
+  }
+}
